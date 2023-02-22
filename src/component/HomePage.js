b2825a59f4d79c01c5ed32e979d494c3174d0e6a
Your Name <you@example.com>
2023-02-22 12:55:13 +0530

    one sapret component create

--- src/component/HomePage.js
+++ src/component/HomePage.js
@@ -1,10 +1,20 @@
 import React from "react";
-import CustomeTable from "./CustomeTable";
+//import CustomeTable from "./CustomeTable";
+import Demo from "../component/demo";
+import Data from "./Data.json";
 
+const columns = [
+  { key: "id", label: "NO" },
+  { key: "name", label: "First Name" },
+  { key: "lastname", label: "Last Name" },
+  { key: "post", label: "Post" },
+  { key: "city", label: "City" },
+];
 const HomePage = () => {
   return (
     <>
-      <CustomeTable />
+      {/* <CustomeTable /> */}
+      <Demo data={Data} columns={columns} />
     </>
   );
 };
---
+++ src/component/demo.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { Table } from "react-bootstrap";
+import PagePagination from "./Pagination";
+const Demo = ({ data, columns }) => {
+  const [sortConfig, setSortConfig] = useState(null);
+  const [showPerPage, setShowPerPage] = useState(10);
+  const [page, setPage] = useState();
+
+  const sortedData = React.useMemo(() => {
+    let sortedData = [...data];
+    if (sortConfig !== null) {
+      sortedData.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortedData;
+  }, [data, sortConfig]);
+
+  const requestSort = (key) => {
+    let direction = "ascending";
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === "ascending"
+    ) {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+  };
+
+  //   pagination start
+  const changeOption = (e) => {
+    const pageValue = Number(e.target.value);
+    setShowPerPage(pageValue);
+  };
+  // pagination end
+  return (
+    <>
+      <Table>
+        <thead>
+          <tr>
+            {columns.map((column) => (
+              <th key={column.key} onClick={() => requestSort(column.key)}>
+                {column.label}
+                {sortConfig &&
+                  sortConfig.key === column.key &&
+                  (sortConfig.direction === "ascending" ? " ▲" : " ▼")}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {sortedData.slice(page * showPerPage, (page + 1) * showPerPage).map((row) => (
+            <tr key={row.id}>
+              {columns.map((column) => (
+                  <td key={column.key}>{row[column.key]}</td>
+                ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <div className="main_pagination">
+        <PagePagination
+          showPerPage={showPerPage}
+          onPagination={(start) => setPage(start / showPerPage)}
+          total={data.length}
+          page={page}
+          setPage={setPage}
+          changeOption={changeOption}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Demo;
